dd50f16020d3facb0416c8b8b20900c3f6f6d629
namelew <diogomaciel.cunha@gmail.com>
2023-04-26 11:39:45 -0300

    adding message struct in queue

--- packages/queue/queue.go
+++ packages/queue/queue.go
@@ -3,11 +3,13 @@ package queue
 import (
 	"errors"
 	"sync"
+
+	"github.com/namelew/RPC/packages/messages"
 )
 
 type node struct {
 	destiny uint64
-	data    interface{}
+	data    messages.Message
 }
 
 type Queue struct {
@@ -21,14 +23,14 @@ func (q *Queue) Check(id uint64) bool {
 	return q.elements[0].destiny == id
 }
 
-func (q *Queue) Dequeue() (interface{}, error) {
+func (q *Queue) Dequeue() (messages.Message, error) {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
 
 	n := len(q.elements)
 
 	if n < 1 {
-		return nil, errors.New("empty queue")
+		return messages.Message{}, errors.New("empty queue")
 	}
 
 	data := q.elements[0].data
@@ -42,7 +44,7 @@ func (q *Queue) Dequeue() (interface{}, error) {
 	return data, nil
 }
 
-func (q *Queue) Enqueue(id uint64, data interface{}) {
+func (q *Queue) Enqueue(id uint64, data messages.Message) {
 	q.mutex.Lock()
 
 	q.elements = append(q.elements, node{id, data})
